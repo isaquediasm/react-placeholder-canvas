8739b83052f423866672705ee3446fb4e81012a2
Travis CI <travis@travis-ci.org>
2019-10-01 22:57:35 +0200

    fix(travis): remove semantic-release from travis

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,2 @@
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
-
-make_semantic_tag() {
-    git checkout master
-    yarn run semantic-release
-}
-
-setup_git
-make_semantic_tag
+GH_TOKEN=caa26998551f8c32bc9001acc9cb8417ebadbd37
+yarn run semantic-release
